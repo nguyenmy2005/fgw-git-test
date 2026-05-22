a11fe29995604d438f268adc4a64b1d07d976859
nguyenmy2005 <nguyenmy2005.org@gmail.com>
2026-05-22 11:07:56 +0700

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World php</title>
+</head>
+<body>
+<p><?php
+/* multiline
+comment */
+echo 'Hello World';
+//single line comment
+?></p>
+
+</body>
+</html>
---
+++ hello_bold.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World php</title>
+</head>
+<body>
+<p><?php echo '<strong>Hello World</strong>'; ?></p>
+
+</body>
+</html>
